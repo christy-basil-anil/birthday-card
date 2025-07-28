4fd2b20f693bd14165c954dc343b34695cdc61d0
Christy Basil Anil <magicianchristy@gmail.com>
2025-07-28 18:13:00 +0530

    Add files via upload

---
+++ birth.js
@@ -0,0 +1,9 @@
+
+function sur() {
+  alert("happy birthday to you ");
+  document.getElementById("bbtn").style.visibility = "hidden";
+  document.getElementById("bcard").style.visibility = "visible";
+
+  console.log("hi")
+}
+
